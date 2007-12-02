839a02968d6a528fa10a8b722b0d33518335fcb3
Benjamin Marty <bluemonkmn@gmail.com>
2007-12-02 15:33:24 +0000

    Add Tutorial4.html for sound and music.

--- SGDK2IDE/About.cs
+++ SGDK2IDE/About.cs
@@ -253,7 +253,7 @@ namespace SGDK2
 
       protected override void OnLoad(EventArgs e)
       {
-         lblVersion.Text = "Version " + Application.ProductVersion + " beta release 1";
+         lblVersion.Text = "Version " + Application.ProductVersion + " beta release 2";
          AssemblyCopyrightAttribute attrCopyright = (AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute), false);
          this.lblCopyright.Text = attrCopyright.Copyright;
 

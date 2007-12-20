bcdcc39e511b1dd926eb6a669c298a5e19bdbf08
Benjamin Marty <bluemonkmn@gmail.com>
2007-12-20 00:25:48 +0000

    Change about dialog to remove "Release Candidate 1" qualifier. Correct default numeric values in frame tweening wizard to use system's decimal separator.

--- SGDK2IDE/About.cs
+++ SGDK2IDE/About.cs
@@ -253,7 +253,7 @@ namespace SGDK2
 
       protected override void OnLoad(EventArgs e)
       {
-         lblVersion.Text = "Version " + Application.ProductVersion + " Release Candidate 1";
+         lblVersion.Text = "Version " + Application.ProductVersion;
          AssemblyCopyrightAttribute attrCopyright = (AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute), false);
          this.lblCopyright.Text = attrCopyright.Copyright;
 

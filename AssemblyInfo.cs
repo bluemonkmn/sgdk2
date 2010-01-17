10c20d541cca8fac6a5dacef50ffc51ea2a55af9
Benjamin Marty <bluemonkmn@gmail.com>
2010-01-17 02:23:36 +0000

    Update version to 2.1.3. Update copyright to 2010. Implement PlayMusic function that cross-fades background music.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -36,10 +36,10 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.1.3.0")]
 
 // Displayed on splash screen
-[assembly: AssemblyFileVersion("2.1.2")]
+[assembly: AssemblyFileVersion("2.1.3")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 

00114a03fa71d3f0de8914e5b938dba064f890b4
Benjamin Marty <bluemonkmn@gmail.com>
2010-05-23 14:27:43 +0000

    Update version to 2.1.5 Avoid error when closing map editor while plan list has focus.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -36,10 +36,10 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.1.4.0")]
+[assembly: AssemblyVersion("2.1.5.0")]
 
 // Displayed on splash screen
-[assembly: AssemblyFileVersion("2.1.4")]
+[assembly: AssemblyFileVersion("2.1.5")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 

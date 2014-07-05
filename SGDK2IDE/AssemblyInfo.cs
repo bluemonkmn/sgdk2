0fa8e558906ded020d4b336bdee0c6d38fd3b605
Benjamin Marty <bluemonkmn@gmail.com>
2014-07-05 17:37:01 +0000

    Revert to .NET 4.0 for compatibility with Windows XP. The ClickOnce installer in 4.5 is still too unreliable anyway. Correct mouse coordinates reported by HTML5 code when game view is not at the top left corner of the web page. Correct the way inactive sprites are removed from a category when deactivated in HTML5 projects. Implement HTML5 (js) version of getSpriteWithin function to retrieve sprite within a plan rectangle. Correct HTML5 (js) addSpriteAtPlan function to mark the created sprite as dynamic, and initialize its parameters to default values. Correct HTML5 (js) code to properly determine tile category membership of animated tile frames. Update copyright date and version number. Update template projects to conform to modified SGDK2 file XML schema.

--- SGDK2IDE/AssemblyInfo.cs
+++ SGDK2IDE/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Scrolling Game Development Kit")]
-[assembly: AssemblyCopyright("Copyright ©2000-2012 Benjamin Marty")]
+[assembly: AssemblyCopyright("Copyright ©2000-2014 Benjamin Marty")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -38,10 +38,10 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.2.9.3")]
+[assembly: AssemblyVersion("2.2.10.1")]
 
 // Displayed on splash screen and about dialog
-[assembly: AssemblyInformationalVersion("2.2.9")]
+[assembly: AssemblyInformationalVersion("2.2.10")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 

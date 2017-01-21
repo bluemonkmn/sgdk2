1b3d378a0b2a5e1d57528475a9f88e3c2bd1e4d9
Benjamin Marty <bluemonkmn@gmail.com>
2017-01-21 21:22:15 +0000

    Update version to 2.3.0.8.

--- SGDK2IDE/Reflect/AssemblyInfo.cs
+++ SGDK2IDE/Reflect/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Scrolling Game Development Kit 2 Runtime Type Reflection")]
 [assembly: AssemblyCompany("Benjamin Marty")]
 [assembly: AssemblyProduct("Scrolling Game Development Kit 2")]
-[assembly: AssemblyCopyright("Copyright ©2006-2012 Benjamin Marty")]
-[assembly: AssemblyVersion("2.3.0.7")]
+[assembly: AssemblyCopyright("Copyright ©2006-2017 Benjamin Marty")]
+[assembly: AssemblyVersion("2.3.0.8")]

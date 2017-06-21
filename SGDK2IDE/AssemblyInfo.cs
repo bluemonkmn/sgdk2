78abedd9345ea617ae366ce2f59a663d7a27f03e
Benjamin Marty <bluemonkmn@gmail.com>
2017-06-21 21:57:38 +0000

    Correct a bug when the map editor attempted to display a layer close to the same size as the game view. An endless loop would cause the scroll bars to disappear and reappear resizing the display to fit.

--- SGDK2IDE/AssemblyInfo.cs
+++ SGDK2IDE/AssemblyInfo.cs
@@ -38,10 +38,10 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.3.0.15")]
+[assembly: AssemblyVersion("2.3.1.0")]
 
 // Displayed on splash screen and about dialog
-[assembly: AssemblyInformationalVersion("2.3.0")]
+[assembly: AssemblyInformationalVersion("2.3.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
